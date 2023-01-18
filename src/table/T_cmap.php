c49dda1e41254ca2849cb98caf4a23b9be2da7fa
ren1244 <n1244506804@hotmail.com>
2023-01-18 12:08:21 +0800

    Add cache mothods for cmap

--- src/table/T_cmap.php
+++ src/table/T_cmap.php
@@ -46,6 +46,14 @@ class T_cmap
     private $numTables;
     private $encodingRecords;
 
+    /** @var string unicode(4 byte) + gid(2 byte) 組成陣列 */
+    private $cache = null;
+
+    /** @var int cache 共有幾筆資料( = strlen(cache) / 6) */
+    private $count;
+
+    private $cacheUTG = [];
+
     public function __construct(TypeReader $reader)
     {
         $this->reader = $reader;
@@ -103,6 +111,84 @@ class T_cmap
             }
         }
         return null;
+    }
+
+    /**
+     * 產生可用於 cache 的字串
+     *
+     * @return string
+     */
+    public function createCache()
+    {
+        if (($unicodeToGid = $this->getUnicodeToGid()) === null) {
+            throw new Exception('cannot find unicode to gid mapping');
+        }
+        ksort($unicodeToGid, SORT_NUMERIC);
+        $cache = '';
+        foreach ($unicodeToGid as $code => $gid) {
+            $cache .= pack('Nn', $code, $gid);
+        }
+        return $cache;
+    }
+
+    /**
+     * 讀取 cache 字串
+     *
+     * @param  string $cache
+     * @return void
+     */
+    public function loadCache(string $cache)
+    {
+        $this->cache = $cache;
+        $this->count = intval(strlen($cache) / 6);
+    }
+
+    /**
+     * 取得 unicode 對應的 GID
+     * 建議先用 loadCache 載入事先產生的 cache
+     * （由於 createCache 計算量較大，應事先計算）
+     *
+     * @param  int $unicode
+     * @return int GID 若找不到時會回傳 0
+     */
+    public function getGid(int $unicode)
+    {
+        if ($this->cache === null) {
+            $this->loadCache($this->createCache());
+        }
+        if(isset($this->cacheUTG[$unicode])) {
+            return $this->cacheUTG[$unicode];
+        }
+        $start = 0;
+        $end = $this->count - 1;
+        $data = $this->cache;
+        $pos = $start * 6;
+        $startVal = ord($data[$pos]) << 24 | ord($data[$pos + 1]) << 16 | ord($data[$pos + 2]) << 8 | ord($data[$pos + 3]);
+        if ($startVal === $unicode) {
+            return $this->cacheUTG[$unicode] = ord($data[$pos + 4]) << 8 | ord($data[$pos + 5]);
+        }
+        $pos = $end * 6;
+        $endVal = ord($data[$pos]) << 24 | ord($data[$pos + 1]) << 16 | ord($data[$pos + 2]) << 8 | ord($data[$pos + 3]);
+        if ($endVal === $unicode) {
+            return $this->cacheUTG[$unicode] = ord($data[$pos + 4]) << 8 | ord($data[$pos + 5]);
+        }
+        if ($unicode < $startVal || $endVal < $unicode) {
+            return $this->cacheUTG[$unicode] = 0;
+        }
+        // 用二分搜尋法找
+        while ($start < $end) {
+            $mid = $start + $end + 1 >> 1;
+            $pos = $mid * 6;
+            $x = ord($data[$pos]) << 24 | ord($data[$pos + 1]) << 16 | ord($data[$pos + 2]) << 8 | ord($data[$pos + 3]);
+            if ($x < $unicode) {
+                $start = $mid;
+            } elseif ($unicode < $x) {
+                $end = $mid - 1;
+            } else {
+                return $this->cacheUTG[$unicode] = ord($data[$pos + 4]) << 8 | ord($data[$pos + 5]);
+            }
+        }
+        return $this->cacheUTG[$unicode] = 0;
     }
 
     public function __toString()
